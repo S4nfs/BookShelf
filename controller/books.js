08d0db2201c70f4954f5ed257fd1d47603711fb0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-14 21:47:53 +0530

    book controller

--- controller/books.js
+++ controller/books.js
@@ -18,7 +18,7 @@ fs.createReadStream(path.resolve(__dirname, "../public/uploads/", "authors.csv")
     .pipe(csv.parse(options))
     .on('error', error => console.error(error))
     .on('data', row => authdata.push(row))
-    .on('end', () => console.log("Authors->", authdata));
+// .on('end', () => console.log("Authors->", authdata));
 
 fs.createReadStream(path.resolve(__dirname, "../public/uploads/", "books.csv"))
     .pipe(csv.parse(options))
@@ -71,8 +71,87 @@ const findMagByAuthor = (mags, authorId, myMag = []) => {
     return myMag
 }
 
+//get all data
+exports.data = async (req, res) => {
+    try {
+        let data = await createBookShelf(authdata, bdata, mdata)
+        return res.render('index', { data: data })
+    } catch (error) {
+        return res.render('404', error)
+    }
+}
+//get books & magazines by ISBN number
+exports.getByISBNlist = async (req, res, next) => {
+    try {
+        let data = {};
+        const { isbn } = req.body;
+        foundB = await bdata.find((book) => book.isbn == isbn.trim())
+        if (typeof (foundB) != "undefined") { data.book = foundB }
+        foundM = await mdata.find((mag) => mag.isbn == isbn.trim())
+        if (typeof (foundM) != "undefined") { data.magazine = foundM }
+        return res.render('isbn', { data: data })
+    } catch (error) {
+        return res.render('404', error)
+    }
+}
+// exports.getByISBNlistLink = async (req, res, next) => {
+//     try {
+//         let data;
+//         const { isbn } = req.params;
+//         data = await bdata.find((book) => book.isbn === isbn)
+//         data = await mdata.find((mag) => mag.isbn === isbn)
+//         return res.render('isbn', { data: data })
+//     } catch (error) {
+//         return res.render('404', error)
+//     }
+// }
 
-exports.books = (req, res) => {
-    var data = createBookShelf(authdata, bdata, mdata)
-    res.render('index', data)
+exports.getAuthorsBook = async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        let data = await createBookShelf(authdata, bdata, mdata).find((authObj) => authObj.email == email)
+        return res.render('author', { data: data })
+    } catch (error) {
+        return res.render('404', error)
+    }
+}
+
+//add a book
+exports.addBook = async (req, res, next) => {
+    try {
+        if (Object.keys(req.body).length < 4) {
+            return res.render('404')
+        } else {
+            const { title, isbn, authors, description } = req.body;
+            await csv.writeToStream(fs.createWriteStream(path.resolve(__dirname, "../public/uploads/", "books.csv"),
+                { flags: 'a' }),
+                [
+                    [""],
+                    [title, isbn, authors, description]
+                ], { headers: false }, { ignoreEmpty: true }).on('error', error => console.error(error))
+            return res.redirect('/')
+        }
+    } catch (error) {
+        return res.render('404', { error: error })
+    }
+}
+
+//add a magazine
+exports.addMagazine = async (req, res, next) => {
+    try {
+        if (Object.keys(req.body).length < 4) {
+            return res.render('404')
+        } else {
+            const { title, isbn, authors, publishedAt } = req.body;
+            await csv.writeToStream(fs.createWriteStream(path.resolve(__dirname, "../public/uploads/", "magazines.csv"),
+                { flags: 'a' }),
+                [
+                    [""],
+                    [title, isbn, authors, publishedAt]
+                ], { headers: false }, { ignoreEmpty: true }).on('error', error => console.error(error))
+            return res.redirect('/')
+        }
+    } catch (error) {
+        return res.render('404', { error: error })
+    }
 }
