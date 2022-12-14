5b83fe676b8847ed0742c5fb3897c40e3b3bcb30
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-14 21:48:15 +0530

    http routes

--- routes/books.route.js
+++ routes/books.route.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const { books } = require('../controller/books');
+const { data, getByISBNlist, getByISBNlistLink, getAuthorsBook, addBook, addMagazine } = require('../controller/books');
 
+router.get('/', data);
+router.post('/isbn', getByISBNlist);
+router.get('/isbn/:isbn', getByISBNlistLink);
+router.post('/author', getAuthorsBook);
+router.post('/addbook', addBook);
+router.post('/addmagazine', addMagazine);
 
-router.get('/', books)
-
-// router.get('/books', (req, res, next) => {
-//     res.render('index');
-// })
 
 
 module.exports = router;
